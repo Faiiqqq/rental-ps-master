dbc58864bb1f981159f6e79e8491e2cf47d1989b
Faiiqqq <muhammadfaiqjr@gmail.com>
2026-02-13 06:19:36 +0700

    Final Project Rental PS - Laravel 12 (Full Fitur: Trigger, Stored Proc, Laporan)

--- resources/views/laporan/index.blade.php
+++ resources/views/laporan/index.blade.php
@@ -20,14 +20,20 @@
                     <div>
                         <label class="block text-sm font-medium text-gray-700 mb-1">Dari Tanggal</label>
                         <input type="date" name="tgl_awal" required value="{{ date('Y-m-01') }}"
-                            class="w-full rounded-lg border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+                            class="w-full rounded text-sm p-2
+                                    bg-linear-to-b from-gray-50 to-gray-100
+                                    border border-gray-300
+                                    hover:border-gray-400 focus:ring-blue-500 focus:border-blue-500">
                     </div>
 
                     {{-- Tanggal Akhir --}}
                     <div>
                         <label class="block text-sm font-medium text-gray-700 mb-1">Sampai Tanggal</label>
                         <input type="date" name="tgl_akhir" required value="{{ date('Y-m-d') }}"
-                            class="w-full rounded-lg border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+                            class="w-full rounded text-sm p-2
+                                    bg-linear-to-b from-gray-50 to-gray-100
+                                    border border-gray-300
+                                    hover:border-gray-400 focus:ring-blue-500 focus:border-blue-500">
                     </div>
 
                     <button type="submit" 
